e9cedea21678bc6e7874b0a71e7160736ac82764
Pamt-o <80077413+Pamt-o@users.noreply.github.com>
2021-09-18 16:25:32 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Pâmela Albuquerque!"
+    return "Pâmela Albuquerque"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
